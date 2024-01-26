8330bbf311ad612f5da3a149c8f6d93a3e4719f8
Manish Ray <manishray2233@gmail.com>
2024-01-26 21:39:32 +0530

    Done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Button from "./Button";
 
+/* Hero section may not be responsive in the mobile view */
+
 const Hero = () => {
   return (
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
