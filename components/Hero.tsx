eb38dc307ddcf5ec954a15836c0d2504adc1f101
Manish Ray <manishray2233@gmail.com>
2024-01-26 21:38:44 +0530

    Modified Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -62,7 +62,7 @@ const Hero = () => {
               <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-white">Aguas Calientes</p>
+            <p className="bold-20 text-white">Bargarh</p>
           </div>
 
           <div className="flexBetween">
