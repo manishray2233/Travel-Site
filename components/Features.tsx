de7007c2ed1a3621e3d3d6b5c63748b904cb75d3
Manish Ray <manishray2233@gmail.com>
2024-01-30 11:02:30 +0530

    Working

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { FEATURES } from "@/constants";
+import Image from "next/image";
+import React from "react";
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            src="/phone.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone"
+          />
+        </div>
+
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
+          <div className="relative">
+            <Image
+              src="/camp.svg"
+              alt="camp"
+              width={50}
+              height={50}
+              className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
+            />
+            <h2 className="bold-40 lg:bold-64">Our Features</h2>
+          </div>
+
+          <ul>
+            {FEATURES.map((feature) => (
+              <FeatureIrem />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const FeatureIrem = () => {
+  return(
+    <div>
+      
+    </div>
   )
 }
 
-export default Features
+export default Features;
