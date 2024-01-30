13d74710847fc159c4b5fa9931d44413b68538dd
Manish Ray <manishray2233@gmail.com>
2024-01-30 21:58:53 +0530

    Working of Features section

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const Features = () => {
   return (
-    <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-      <div className="max-container padding-container relative w-full justify-end">
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full flex justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
@@ -28,9 +28,14 @@ const Features = () => {
             <h2 className="bold-40 lg:bold-64">Our Features</h2>
           </div>
 
-          <ul>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20">
             {FEATURES.map((feature) => (
-              <FeatureIrem />
+              <FeatureIrem
+                key={feature.title}
+                title={feature.title}
+                icon={feature.icon}
+                description={feature.description}
+              />
             ))}
           </ul>
         </div>
@@ -39,12 +44,26 @@ const Features = () => {
   );
 };
 
-const FeatureIrem = () => {
-  return(
-    <div>
-      
-    </div>
-  )
-}
+type FeatureItem = {
+  title: string;
+  icon: string;
+  description: string;
+};
+
+const FeatureIrem = ({ title, icon, description }: FeatureItem) => {
+  return (
+    <li className="flex w-full flex-1 flex-col items-start">
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
+        <Image src={icon} alt="map" width={28} height={28} />
+      </div>
+
+      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
+
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+        {description}
+      </p>
+    </li>
+  );
+};
 
 export default Features;
